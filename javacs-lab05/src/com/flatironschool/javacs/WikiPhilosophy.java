6ffa75bb38bbf51e41d5519847238b7271cf0031
Anwaar Bastien <Anwaar@Anwaars-MBP.home>
2016-05-25 16:20:31 -0400

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -11,41 +11,64 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
+
 	final static WikiFetcher wf = new WikiFetcher();
-	
+	final static String check = "https://en.wikipedia.org/wiki/Philosophy";
+	static ArrayList<String> list = new ArrayList<String>();
+	static int count = 0;
+
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
 	 * 
 	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
 	 * 
-	 * 1. Clicking on the first non-parenthesized, non-italicized link
-     * 2. Ignoring external links, links to the current page, or red links
-     * 3. Stopping when reaching "Philosophy", a page with no links or a page
-     *    that does not exist, or when a loop occurs
+	 * 1. Clicking on the first non-parenthesized, non-italicized link 2.
+	 * Ignoring external links, links to the current page, or red links 3.
+	 * Stopping when reaching "Philosophy", a page with no links or a page that
+	 * does not exist, or when a loop occurs
 	 * 
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
+		boolean attempt = crawl("https://en.wikipedia.org/wiki/Java_(programming_language)");
+		if(attempt) {
+			System.out.println("Success");
+			return ;
+		}
+		System.out.println("Failure");
+	}
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+	private static boolean crawl(String target) throws IOException {
+		String url = target;
 		Elements paragraphs = wf.fetchWikipedia(url);
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
+		for (Element paras : paragraphs) {
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+			Iterable<Node> iter = new WikiNodeIterable(paras);
+			for (Node node : iter) {
+				if (node instanceof Element
+						&& ((Element) node).tagName().equals("a")
+						&& ((Element) node).parent().tagName().equals("p")) {
+					count++;
+					list.add(((Element) node).absUrl("href"));
+					System.out.println(((Element) node).absUrl("href"));
+					if (((Element) node).absUrl("href").equals(check)) {
+						System.out.println(list);
+						return true;
+					} else {
+						crawl(((Element) node).absUrl("href"));
+						break;
+					}
+				}
+			}
+			if (count != 0) {
+				break;
+			}
+		}
+		if (count == 0) {
+			return false;
+		}
+		return true;
 	}
 }
